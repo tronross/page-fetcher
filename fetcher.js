da8bf3137a279a08820d0d6853f8bf02412d1614
tronross <john_ross@analogforever.ca>
2022-10-21 18:05:47 -0400

    Got rid of net (redundant); require fs

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,11 @@
-const net = require('net');
 const request = require('request');
+const fs = require('fs');
 
 
-const connect = function(name) {
-  const conn = net.createConnection({
-    host: IP,
-    port: PORT,
-  });
-}
+
+
+request('http://google.com', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
