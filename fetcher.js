e65d2c9fa827162081fa1532eea50d24e3401e43
tronross <john_ross@analogforever.ca>
2022-10-21 21:32:49 -0400

    Working, extracts arguments from CLI, only logs errors if not null and staus code if not 200

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,27 @@
 const request = require('request');
 const fs = require('fs');
 
+const url = process.argv[2];
+const localPath = process.argv[3];
 
+const fetcher = function(url, localPath) {
+  
+  request(url, (error, response, body) => {
+    // Print the error if one occurred
+    if (error !== null) {
+      console.log('error:', error);
+    }
+    // Print the response status code if not 200
+    if (response.statusCode !== 200) {
+      console.log('statusCode:', response && response.statusCode);
+    }
+    
+    fs.writeFile(localPath, body, () => {
+      const fileStats = fs.statSync(localPath);
+      const size = fileStats.size;
+      console.log(`Successfully downloaded and saved ${size} bytes to ${localPath}`);
+    });
+  });
+};
 
-
-request('http://google.com', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+fetcher(url, localPath);
